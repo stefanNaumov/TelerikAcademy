e2690e5e2988f1771c2a017c71d1cf0697fff509
stefanNaumov <stefannaumow@gmail.com>
2014-06-27 10:35:50 +0300

    Functions and function expressions

---
+++ JS-OOP/01-FuncsAndFuncExpressions/01-FuncsAndFuncExpressions/01-Module.js
@@ -0,0 +1,61 @@
+﻿var domModule = (function() {
+
+    var elementsBuffer = {};
+
+    function appendChild(element, container) {
+
+        var container = document.querySelector(container);
+        container.appendChild(element);
+    }
+
+    function removeElement(parentSelector,elementToRemove) {
+
+        var parent = document.querySelector(parentSelector);
+        
+        var elementToRemove = document.querySelector(elementToRemove);
+
+        parent.removeChild(elementToRemove);
+    }
+
+    function addHandler(selector, eventType, eventListener) {
+
+        var objToListen = document.querySelector(selector);
+        objToListen.addEventListener(eventType, eventListener);
+    }
+
+    function appendToBuffer(currentContainer, elementToAdd) {
+
+        if (!elementsBuffer[currentContainer]) {
+            elementsBuffer[currentContainer] = document.createDocumentFragment();
+        }
+
+        elementsBuffer[currentContainer].appendChild(elementToAdd);
+
+        if (elementsBuffer[currentContainer].childNodes.length >= 100) {
+
+            var containerElement = document.querySelector(currentContainer);
+            containerElement.appendChild(elementsBuffer[currentContainer]);
+        }
+
+    }
+
+    function getByCssSelector(selector) {
+        //a little hack
+        if (selector.indexOf('#') > 0) {
+            return document.getElementById(selector);
+        }
+        else if (selector.indexOf('.') > 0) {
+            return document.getElementsByClassName(selector);
+        }
+        else {
+            return document.getElementsByTagName(selector);
+        }
+    }
+    return {
+        appendChild: appendChild,
+        removeElement: removeElement,
+        addHandler: addHandler,
+        appendToBuffer: appendToBuffer,
+        findByCssSelector: getByCssSelector
+    }
+}());
---
+++ JS-OOP/01-FuncsAndFuncExpressions/01-FuncsAndFuncExpressions/02-MovingObjsModule.js
@@ -0,0 +1,46 @@
+﻿var movingShapesModule = (function () {
+
+    'use strict';
+
+    function generateDivElement() {
+
+        var div = document.createElement('div');
+        div.innerText = 'div';
+        div.style.position = 'absolute';
+        div.style.display = 'inline-block';
+        div.style.left = randomGenerator(50, 500) + 'px';
+        div.style.top = randomGenerator(10, 100) + 'px';
+        div.style.background = generateColor();
+        div.style.fontSize = randomGenerator(8, 26) + 'px';
+        div.style.padding = randomGenerator(1, 25) + 'px';
+
+        return div;
+    }
+
+    function generateColor() {
+        var red = randomGenerator(0, 255);
+        var green = randomGenerator(0, 255);
+        var blue = randomGenerator(0, 255);
+
+        return 'rgb(' + red + ',' + green + ',' + blue + ');';
+    }
+    function addShape(shape) {
+
+        var currDiv = generateDivElement();
+
+        if (shape === 'rect') {
+
+        }
+
+        if (shape === 'ellipse') {
+
+        }
+    }
+
+    function randomGenerator(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    return {
+        addShape:addShape
+    }
+}());
