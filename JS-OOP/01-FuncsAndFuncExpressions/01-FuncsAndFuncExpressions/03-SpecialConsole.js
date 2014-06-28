1eab4429cfc01fa2b7cd62c9c6a68192632ac150
stefanNaumov <stefannaumow@gmail.com>
2014-06-28 20:30:33 +0300

    JS OOP

---
+++ JS-OOP/01-FuncsAndFuncExpressions/01-FuncsAndFuncExpressions/03-SpecialConsole.js
@@ -0,0 +1,28 @@
+﻿var specialConsole = (function () {
+
+    function formatInput(args) {
+
+        if (args.length > 1) {
+
+            var messageStr = args[0];
+
+            for (var i = 1; i < args.length; i++) {
+                messageStr = messageStr.replace('{' + (i - 1) + '}', args[i]);
+            }
+            return messageStr;
+        }
+        return args[0];
+    }
+
+    return {
+        writeLine: function () {
+            console.log(formatInput(arguments));
+        },
+        warn: function(){
+            console.log(formatInput(arguments))
+        },
+        error: function () {
+            console.log(formatInput(arguments));
+        }
+    }
+}());
---
+++ JS-OOP/02-ClassicalOOP/02-ClassicalOOP/CanvasShapes.js
@@ -0,0 +1,33 @@
+﻿var CanvasShapes = function (canvasID) {
+    var ctx = document.getElementById(canvasID).getContext('2d');
+    
+    function drawRect(x, y, width, height, color) {
+        ctx.beginPath();
+        ctx.rect(x, y, width, height);
+        ctx.fillStyle = color;
+        ctx.fill();
+        ctx.stroke();
+    }
+
+    function drawCircle(x, y, radius, color) {
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, 2 * Math.PI);
+        ctx.fillStyle = color;
+        ctx.fill();
+        ctx.stroke();
+    }
+
+    function drawLine(xStart,yStart,xEnd,yEnd,strokeColor) {
+        ctx.beginPath();
+        ctx.moveTo(xStart, yStart);
+        ctx.lineTo(xEnd, yEnd);
+        ctx.strokeStyle = strokeColor;
+        ctx.stroke();
+    }
+
+    return {
+        drawRectangle: drawRect,
+        drawCircle: drawCircle,
+        drawLine:drawLine
+    }
+};
