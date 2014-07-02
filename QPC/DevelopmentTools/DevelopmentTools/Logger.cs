c267f420024ef130a0c5037880d68f012be6ffff
stefanNaumov <stefannaumow@gmail.com>
2014-07-02 21:42:59 +0300

    dev tools

---
+++ QPC/DevelopmentTools/DevelopmentTools/Logger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+using log4net.Appender;
+using log4net.Layout;
+using log4net.Core;
+using System.IO;
+namespace DevelopmentTools
+{
+    //Obfuscator used on this assembly - check the ObfuscatedAssembly folder
+    class Logger
+    {
+        private static readonly ILog log =
+            LogManager.GetLogger(typeof(Logger));
+
+        static void Main()
+        {
+            var fileAppender = new FileAppender();
+            fileAppender.File = "log.txt";
+            fileAppender.AppendToFile = true;
+            fileAppender.Layout = new SimpleLayout();
+            fileAppender.Threshold = Level.Warn;
+            fileAppender.ActivateOptions();
+            BasicConfigurator.Configure(fileAppender);
+
+            try
+            {
+                File.ReadAllLines(@"124334325.txt");
+            }
+            catch (FileNotFoundException ex)
+            {
+                log.Fatal(ex);
+            }
+            log.Info("Some info");
+            log.Warn("Warning!");
+        }
+    }
+}
---
+++ QPC/DevelopmentTools/DevelopmentTools/T4-Test.cs
@@ -0,0 +1,4 @@
+﻿	public int Item 0 {get; private set;}
+		public int Item 1 {get; private set;}
+		public int Item 2 {get; private set;}
+	
