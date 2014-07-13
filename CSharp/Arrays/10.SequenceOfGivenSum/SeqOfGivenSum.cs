f176179b77fe31885a2e9bd10e02748451cef588
stefanNaumov <stefannaumow@gmail.com>
2014-07-13 21:39:48 +0300

    C# Arrays HW - all tasks

---
+++ CSharp/Arrays/10.SequenceOfGivenSum/SeqOfGivenSum.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Write a program that finds in given array of integers a sequence of given sum S (if present). 
+//Example:	 {4, 3, 1, 4, 2, 5, 8}, S=11  {4, 2, 5}
+namespace _10.SequenceOfGivenSum
+{
+    class SeqOfGivenSum
+    {
+        static void Main()
+        {
+            int[] array = GenerateRandomArray(20, 50);
+
+            //The following hardcoded array is used to check for particular boundary problems
+            // in this case for S = 11
+            //int[] arr = new int[] { 11,4, 3, 1, 4, 2, 5, 8, 3, 11 };
+
+            Console.Write("Enter sum S: ");
+            int sum = int.Parse(Console.ReadLine());
+            List<List<int>> sumSequences = FindSequenceOfSum(array, sum);
+            if (sumSequences.Count > 0)
+            {
+                Console.WriteLine("Subsequences with sum equal to input value");
+                for (int i = 0; i < sumSequences.Count; i++)
+                {
+                    List<int> currSeq = sumSequences[i];
+
+                    for (int j = 0; j < currSeq.Count; j++)
+                    {
+                        Console.Write(currSeq[j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("There are no elements with this sum in the array");
+            }
+            
+        }
+
+        static int[] GenerateRandomArray(int minSize, int maxSize)
+        {
+            Random generator = new Random();
+
+            int arraySize = generator.Next(minSize, maxSize);
+            int[] array = new int[arraySize];
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                array[i] = generator.Next(1, 10);
+            }
+
+            return array;
+        }
+
+        static List<List<int>> FindSequenceOfSum(int[] array, int sum)
+        {
+            List<List<int>> allSequences = new List<List<int>>();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                List<int> currentSequence = new List<int>();
+                int currentSum = array[i];
+                currentSequence.Add(array[i]);
+
+                if (currentSum == sum)
+                {
+                    allSequences.Add(currentSequence);
+                }
+                else if (currentSum < sum && i < array.Length - 1)
+                {
+                    for (int j = i + 1; j < array.Length; j++)
+                    {
+                        currentSum += array[j];
+                        currentSequence.Add(array[j]);
+
+                        if (currentSum == sum)
+                        {
+                            allSequences.Add(currentSequence);
+                            break;
+                        }
+                    }
+                }
+                
+            }
+
+            return allSequences;
+        }
+    }
+}
---
+++ CSharp/Arrays/11.BinarySearch/BinarySearch.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Write a program that finds the index of given element in a sorted array of integers by using the binary search algorithm 
+//(find it in Wikipedia).
+
+namespace _11.BinarySearch
+{
+    class BinarySearch
+    {
+        static void Main()
+        {
+            int[] array = GenerateRandomArray(400, 2000);
+            //int[] arr = new int[] { 11,4, 3, 1, 4, 2, 5, 8, 3, 11 };
+            Array.Sort(array);
+            int searchKey = 150;
+            Console.Write("Index using recursive Binary Search algorithm: ");
+            Console.WriteLine(FindIndexRecursive(array, searchKey, 0, array.Length));
+            Console.Write("Index using iterative Binary Search algorithm: ");
+            Console.WriteLine(FindIndexIterative(array, searchKey, 0, array.Length));
+        }
+
+        static int[] GenerateRandomArray(int minSize, int maxSize)
+        {
+            Random generator = new Random();
+
+            int arraySize = generator.Next(minSize, maxSize);
+            int[] array = new int[arraySize];
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                array[i] = generator.Next(1, 500);
+            }
+
+            return array;
+        }
+        
+        static int FindIndexRecursive(int[] sortedArray, int key, int minIndex, int maxIndex)
+        {
+            //No such element found
+            if (maxIndex < minIndex)
+            {
+                return -1;
+            }
+            else
+            {
+                int middleIndex = (maxIndex + minIndex) / 2;
+
+                if (sortedArray[middleIndex] > key)
+                {
+                    return FindIndexRecursive(sortedArray, key, minIndex, middleIndex - 1);
+                }
+                else if (sortedArray[middleIndex] < key)
+                {
+                    return FindIndexRecursive(sortedArray, key, middleIndex + 1, maxIndex);
+                }
+                else
+                {
+                    return middleIndex;
+                }
+            }
+        }
+
+        static int FindIndexIterative(int[] sortedArr, int key, int minIndex, int maxIndex)
+        {
+            
+            while (true)
+            {
+                int middleIndex = (minIndex + maxIndex) / 2;
+
+                if (minIndex > maxIndex)
+                {
+                    return -1;
+                }
+
+                if (sortedArr[middleIndex] < key)
+                {
+                    minIndex = middleIndex + 1;
+                }
+                else if (sortedArr[middleIndex] > key)
+                {
+                    maxIndex = middleIndex - 1;
+                }
+                else
+                {
+                    return middleIndex;
+                }
+            }
+        }
+    }
+}
