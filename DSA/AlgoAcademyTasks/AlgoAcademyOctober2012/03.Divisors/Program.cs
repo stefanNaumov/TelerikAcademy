5150f3f98cae2ba4c986cfc28965198e6aa6ea32
stefanNaumov <stefannaumow@gmail.com>
2014-08-16 12:39:58 +0300

    solved task03- Divisors

--- DSA/AlgoAcademyTasks/AlgoAcademyOctober2012/03.Divisors/Program.cs
+++ DSA/AlgoAcademyTasks/AlgoAcademyOctober2012/03.Divisors/Program.cs
@@ -22,11 +22,90 @@ namespace Divisors
                 numbers[i] = int.Parse(Console.ReadLine());
             }
 
+            GetPermutations(numbers, 0);
+
+            //for (int i = 0; i < permutations.Count; i++)
+            //{
+            //    Console.Write(permutations[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            int result = GetNumWithMinDivisors(permutations);
+
+            Console.WriteLine(result);
         }
 
-        static void GetPermutations()
+        static void GetPermutations(int[] numbersArr, int index)
         {
-            
+            int size = numbersArr.Length;
+            //TODO: fix, too stupid done this way
+            if (index == size)
+            {
+                string numToStr = "";
+                for (int i = 0; i < numbersArr.Length; i++)
+                {
+                    numToStr += numbersArr[i];
+                }
+                int resultNum = int.Parse(numToStr);
+
+                permutations.Add(resultNum);
+
+                return;
+            }
+            else
+            {
+                for (int i = index; i < size; i++)
+                {
+                    int temp = numbersArr[i];
+                    numbersArr[i] = numbersArr[index];
+                    numbersArr[index] = temp;
+
+                    GetPermutations(numbersArr, index + 1);
+
+                    temp = numbersArr[i];
+                    numbersArr[i] = numbersArr[index];
+                    numbersArr[index] = temp;
+                }
+            }
+
+        }
+
+        static long CountDivisors(int number)
+        {
+            long counter = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    counter++;
+                }    
+            }
+
+            return counter;
+        }
+
+        static int GetNumWithMinDivisors(List<int> numbers)
+        {
+            long minDivisorsCount = int.MaxValue;
+            int result = -1;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                long currDivisors = CountDivisors(numbers[i]);
+
+                if (currDivisors == minDivisorsCount)
+                {
+                    result = Math.Min(result, numbers[i]);
+                }
+                if (currDivisors < minDivisorsCount)
+                {
+                    minDivisorsCount = currDivisors;
+                    result = numbers[i];
+                }
+            }
+
+            return result;
         }
     }
 }
