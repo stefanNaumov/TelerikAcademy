7562551bad160490ad570aa457883bcf1bb81288
stefanNaumov <stefannaumow@gmail.com>
2014-08-27 18:15:17 +0300

    added ADO.Net-HWTasks

---
+++ DB/ADO.Net/01.RetrieveNumOfRows/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Data.SqlClient;
+
+namespace RetrieveNumOfRows
+{
+    class Program
+    {
+        static void Main()
+        {
+            SqlConnection sqlCon = new SqlConnection("Server=.;"
+                + "Database=Northwind; Integrated Security=true");
+
+            sqlCon.Open();
+
+            using (sqlCon)
+            {
+                
+            }
+        }
+    }
+}
