795721a68ca1a24da387fccffe618d4693fed730
stefanNaumov <stefannaumow@gmail.com>
2014-08-29 20:21:45 +0300

    added EntityFramework-HWTasks

---
+++ DB/EntityFramework/04.SelectNativeSQL/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _04.SelectNativeSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NorthwindEntities entities = new NorthwindEntities();
+
+            using (entities)
+            {
+                int orderDate = 1997;
+                string shipCountry = "Canada";
+                object[] parameters = {orderDate, shipCountry};
+
+                string query = @"SELECT c.CompanyName FROM Customers c " +
+                        "INNER JOIN Orders o ON o.CustomerID = c.CustomerID " +
+                        "WHERE (YEAR(o.OrderDate) = {0} AND o.ShipCountry = {1});";
+                
+                var queryResult = entities.Database.SqlQuery<string>(query, parameters);
+
+                foreach (var result in queryResult)
+                {
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
---
+++ DB/EntityFramework/05.FindAllSales/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Write a method that finds all the sales by specified region and period (start / end dates).
+
+namespace _05.FindAllSales
+{
+    class Program
+    {
+        static void Main()
+        {
+            FindSales("Sofia", 1997, 2000);
+        }
+
+        private static void FindSales(string region, int startDate, int endDate)
+        {
+            NorthwindEntities entities = new NorthwindEntities();
+
+            var queryResult = from order in entities.Orders
+                        where order.ShipRegion == region
+                        && order.OrderDate.Value.Year > startDate
+                        && order.OrderDate.Value.Year < endDate
+                        select order;
+            if (queryResult.Count() == 0)
+            {
+                Console.WriteLine("No such element");
+            }
+
+            foreach (var order in queryResult)
+            {
+                Console.WriteLine("Region: " + order.ShipRegion + "Date: " + order.OrderDate);
+            }
+        }
+    }
+}
