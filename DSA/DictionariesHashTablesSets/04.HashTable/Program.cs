70bb7bdc2ebb32190b2fddce0139caeeddeb219c
stefanNaumov <stefannaumow@gmail.com>
2014-08-31 01:16:49 +0300

    added DictionaryHashTablesAndSets

--- DSA/DictionariesHashTablesSets/04.HashTable/Program.cs
+++ DSA/DictionariesHashTablesSets/04.HashTable/Program.cs
@@ -6,28 +6,46 @@ using System.Threading.Tasks;
 using System.Collections;
 using System.Collections.ObjectModel;
 
+//Implement the data structure "hash table" in a class HashTable<K,T>. 
+//Keep the data in array of lists of key-value pairs (LinkedList<KeyValuePair<K,T>>[]) 
+//with initial capacity of 16. 
+//When the hash table load runs over 75%, perform resizing to 2 times larger capacity. 
+//Implement the following methods and properties: Add(key, value), 
+//Find(key)value, Remove( key), Count, Clear(), this[], Keys.
+//Try to make the hash table to support iterating over its elements with foreach.
+
 namespace HashTable
 {
-    class Program
+    public class Program
     {
         static void Main()
         {
-           
+            HashTable<int, int> hashTable = new HashTable<int, int>();
+
+            for (int i = 0; i < 200000; i++)
+            {
+                hashTable.Add(i, (i + 1) * i);
+            }
+
+            Console.WriteLine(hashTable.FindValue(250));
+            hashTable.Clear();
+            Console.WriteLine(hashTable.FindValue(250));
         }
     }
-    public class HashTable<K,T>
+
+    public class HashTable<K,T> : IEnumerable
     {
         private const int defaultCapacity = 16;
         private int elementsCount;
         private int capacity;
         LinkedList<KeyValuePair<K, T>>[] list;
-        //constructor
+       
         public HashTable()
         {
             this.list = new LinkedList<KeyValuePair<K, T>>[defaultCapacity];
             this.capacity = defaultCapacity;
         }
-        //properties
+        
         public int Count 
         {
             get { return this.elementsCount; }
@@ -36,43 +54,117 @@ namespace HashTable
         {
             get { return this.capacity; }
         }
-        //methods
+
+        public T this[K indexKey]
+        {
+            get 
+            {
+                return this.FindValue(indexKey);
+            }
+            set
+            {
+                this.Add(indexKey, value);
+            }
+        }
+
+        public List<K> GetKeys()
+        {
+            if (this.list.Length < 1)
+            {
+                throw new ArgumentException("Cannot get keys from an empty HashTable<K,T>!");
+            }
+
+            List<K> keysList = new List<K>();
+
+            foreach (var pair in this.GetLinkedList(0))
+            {
+                keysList.Add(pair.Key);
+            }
+
+            return keysList;
+        }
+        //TODO:Implement
+        public void Remove(T key)
+        {
+
+        }
+
+        public void Clear()
+        {
+            this.list = new LinkedList<KeyValuePair<K, T>>[defaultCapacity];
+        }
+
+        public T FindValue(K key)
+        {
+            int index = this.GetIndex(key);
+            LinkedList<KeyValuePair<K, T>> linkedList = this.GetLinkedList(index);
+
+            foreach (var pair in linkedList)
+            {
+                if (pair.Key.Equals(key))
+                {
+                    return pair.Value;
+                }
+            }
+
+            return default(T);
+        }
+
         public void Add(K key, T value)
         {
-            KeyValuePair<K, T> currentPair = new KeyValuePair<K, T>(key, value);
+            
             if (this.elementsCount >= this.capacity * 0.75)
             {
-                //implement AutoGrow()
+                this.AutoGrow();
             }
-            int index = Index(key);
+            int index = GetIndex(key);
+            KeyValuePair<K, T> currentPair = new KeyValuePair<K, T>(key, value);
+            
+            LinkedList<KeyValuePair<K, T>> linkedList = this.GetLinkedList(index);
+            
+            linkedList.AddLast(currentPair);
+
+            this.elementsCount++;
         }
-        //CHECK!!!
+        
         private void AutoGrow()
         {
             int doubleCapacity = this.capacity * 2;
             LinkedList<KeyValuePair<K, T>>[] newList = new LinkedList<KeyValuePair<K, T>>[doubleCapacity];
-            foreach (var pair in this.list)
+
+            for (int i = 0; i < this.Count; i++)
             {
-                foreach (KeyValuePair<K,T> item in pair)
-                {
-                    int index = this.Index(item.Key);
-                    if (newList[index] != null)
-                    {
-                        newList[index].AddLast(item);
-                    }
-                    else
-                    {
-                        newList[index].AddFirst(item);
-                    }
-                }
+                newList[i] = this.list[i];
             }
+            this.capacity = doubleCapacity;
             this.list = newList;
         }
-        // hash function
-        private int Index(K key)
+
+        private LinkedList<KeyValuePair<K, T>> GetLinkedList(int index)
+        {
+            LinkedList<KeyValuePair<K, T>> linkedList = this.list[index];
+
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValuePair<K, T>>();
+                this.list[index] = linkedList;
+            }
+
+            return linkedList;
+        }
+
+        private int GetIndex(K key)
         {
             int index = key.GetHashCode() % this.list.Length;
             return index;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var item in this.list)
+            {
+                yield return item;
+            }
         }
     }
 }
---
+++ DSA/DictionariesHashTablesSets/05.HashSet/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HashTable;
+using System.Collections;
+
+//Implement the data structure "set" in a class HashedSet<T> using your class HashTable<K,T> to hold the elements. 
+//Implement all standard set operations like Add(T), Find(T), Remove(T), Count, Clear(), union and intersect.
+namespace HashSet
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+
+    public class CustomHashSet<T> : IEnumerable
+    {
+        private HashTable<T, int> hashTable;
+
+        public CustomHashSet()
+        {
+            this.hashTable = new HashTable<T, int>();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.hashTable.Count;
+            }
+        }
+
+        public IEnumerable<T> Items
+        {
+            get
+            {
+                return this.hashTable.GetKeys();
+            }
+        }
+
+       
+
+    }
+}
