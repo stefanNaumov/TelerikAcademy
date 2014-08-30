a7267b211f0a3e4983d69a4b3e18e7c56b9b9958
stefanNaumov <stefannaumow@gmail.com>
2014-08-30 12:49:07 +0300

    added EntityFramework-HWTasks

---
+++ DB/EntityFramework/10.GetIncome/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//STORED PROCEDURE IN SQL SERVER
+
+//ALTER PROC [dbo].[usp_GetIncome](@supplierName varchar(50), @startDate date, @endDate date)
+//AS
+//SELECT s.CompanyName, ordDet.UnitPrice * ordDet.Quantity AS Income
+//FROM Suppliers s
+//INNER JOIN Products p
+//ON p.SupplierID = s.SupplierID
+//INNER JOIN [Order Details] ordDet
+//ON ordDet.ProductID = p.ProductID
+//INNER JOIN Orders o
+//ON o.OrderID = ordDet.OrderID
+//WHERE o.OrderDate BETWEEN @startDate AND @endDate
+//AND s.CompanyName = @supplierName
+//ORDER BY Income
+namespace _10.GetIncome
+{
+    class Program
+    {
+        static void Main()
+        {
+            NorthwindEntities entities = new NorthwindEntities();
+
+            using (entities)
+            {
+                var findIncomes = entities.usp_GetIncome("Leka Trading", DateTime.Parse("1995-01-01"), DateTime.Parse("2012-12-21"));
+
+                foreach (var income in findIncomes)
+                {
+                    Console.WriteLine("Company: " + income.CompanyName + " Income: " + income.Income);
+                }
+            }
+        }
+    }
+}
