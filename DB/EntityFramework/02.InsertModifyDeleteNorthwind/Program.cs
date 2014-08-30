a531c8aea11af7dd2e898cb6ddbf6cfa91d52bf1
stefanNaumov <stefannaumow@gmail.com>
2014-08-30 14:06:19 +0300

    .

--- DB/EntityFramework/02.InsertModifyDeleteNorthwind/Program.cs
+++ DB/EntityFramework/02.InsertModifyDeleteNorthwind/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using _01.DbContext;
+using DbContext;
 //Create a DAO class with static methods which provide functionality for inserting, 
 //modifying and deleting customers. Write a testing class.
 
