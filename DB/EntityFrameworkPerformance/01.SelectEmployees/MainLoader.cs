7e27a0ac7ad312757866929ed478da03ee03b325
stefanNaumov <stefannaumow@gmail.com>
2014-09-03 11:41:45 +0300

    added EntityFrameworkPerformance

---
+++ DB/EntityFrameworkPerformance/01.SelectEmployees/MainLoader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Objects;
+using System.Diagnostics;
+using System.Linq;
+
+//Using Entity Framework write a SQL query to 
+//select all employees from the Telerik Academy database 
+//and later print their name, department and town. 
+//Try the both variants: with and without .Include(…). 
+//Compare the number of executed SQL statements and the performance.
+
+
+namespace _01.SelectEmployees
+{
+    class MainLoader
+    {
+        static void Main()
+        {
+            TelerikAcademyEntities entities = new TelerikAcademyEntities();
+
+            using (entities)
+            {
+                var employees = entities.Employees;
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                foreach (var employee in employees)
+                {
+                   // Console.WriteLine(employee.FirstName + " " + employee.DepartmentID + " " + employee.Addresses.Towns.Name);
+                }
+                stopWatch.Stop();
+                Console.Write("Number of items " + employees.Count() + ": ");
+                Console.WriteLine(stopWatch.Elapsed);
+                stopWatch.Reset();
+
+                stopWatch.Start();
+                foreach (var employee in entities.Employees.Include("Departments").Include("Addresses").Include("Addresses.Towns"))
+                {
+                    //Console.WriteLine(employee.FirstName + " " + employee.DepartmentID + " " + employee.Addresses.Towns.Name);
+                }
+                stopWatch.Stop();
+                Console.Write("Number of items " + entities.Employees.Count() + ": ");
+                Console.WriteLine(stopWatch.Elapsed);
+            }
+        }
+    }
+}
---
+++ DB/EntityFrameworkPerformance/02.ToListTest/MainLoader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+//Using Entity Framework write a query that selects all employees from the Telerik Academy database, 
+//then invokes ToList(), then selects their addresses, then invokes ToList(), 
+//then selects their towns, then invokes ToList() and finally checks whether the town is "Sofia". 
+//Rewrite the same in more optimized way and compare the performance.
+
+namespace _02.ToListTest
+{
+    class MainLoader
+    {
+        static void Main()
+        {
+            TelerikAcademyEntities entities = new TelerikAcademyEntities();
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            SlowSelection(entities);
+
+            watch.Stop();
+            Console.WriteLine("Slow selection: " + watch.Elapsed);
+
+            watch.Reset();
+            watch.Start();
+
+            OptimizedConnection(entities);
+
+            watch.Stop();
+            Console.WriteLine("Optimized selection: " + watch.Elapsed);
+        }
+
+        static void SlowSelection(TelerikAcademyEntities entities)
+        {
+            List<Employees> allEmployees = entities.Employees.ToList();
+            List<Addresses> employeesAddresses = (from employee in allEmployees
+                                                  select employee.Addresses).ToList();
+            List<Towns> employeesTowns = (from town in employeesAddresses
+                                          select town.Towns).ToList();
+            var sofiaSelect = from town in employeesTowns
+                              where town.Name == "Sofia"
+                              select town;
+        }
+
+        static void OptimizedConnection(TelerikAcademyEntities entities)
+        {
+            var optimizedSelection = from town in entities.Towns
+                                     where town.Name == "Sofia"
+                                     select town;
+        }
+    }
+}
