1bb6665f173d18f0bb2076491515d8107e9aa3ca
stefanNaumov <stefannaumow@gmail.com>
2014-09-03 18:11:56 +0300

    added DatabasePerformance

---
+++ DB/DatabasePerformance/DatabasePerformance.sql
@@ -0,0 +1,37 @@
+--01-Create a table in SQL Server with 10 000 000 log entries (date + text). 
+--Search in the table by date range. Check the speed (without caching).
+
+--02-Add an index to speed-up the search by date. Test the search speed (after cleaning the cache).
+
+--03-Add a full text index for the text column. Try to search with and without the full-text index and compare the speed.
+
+CREATE TABLE LogEntries
+(
+	LogId int IDENTITY,
+	LogDate date,
+	Description varchar(50)
+)
+
+INSERT INTO LogEntries(LogDate,Description)
+VALUES('2012-08-09','Some Description')
+
+CHECKPOINT; 
+GO 
+DBCC DROPCLEANBUFFERS; 
+GO
+
+DECLARE @Counter int = 0
+WHILE (SELECT COUNT(*) FROM LogEntries) < 1000000
+BEGIN
+  INSERT INTO LogEntries(LogDate, Description)
+  SELECT DATEADD(MONTH, @Counter + 3, LogDate), Description + CONVERT(varchar, @Counter) FROM LogEntries
+  SET @Counter = @Counter + 1
+END
+
+CHECKPOINT; 
+GO 
+DBCC DROPCLEANBUFFERS; 
+GO
+
+SELECT Description FROM LogEntries WHERE LogDate 
+		BETWEEN CONVERT(datetime, '1990-01-01') AND CONVERT(datetime, '1995-01-01');
