60be68c69e0eb127025b7c1cac2a4ae8cf50db77
stefanNaumov <stefannaumow@gmail.com>
2014-09-04 13:00:14 +0300

    added DatabasePerformance

--- DB/DatabasePerformance/DatabasePerformance.sql
+++ DB/DatabasePerformance/DatabasePerformance.sql
@@ -35,3 +35,31 @@ GO
 
 SELECT Description FROM LogEntries WHERE LogDate 
 		BETWEEN CONVERT(datetime, '1990-01-01') AND CONVERT(datetime, '1995-01-01');
+
+CREATE INDEX IDX_LogColIndex ON LogEntries(LogDate);
+
+CHECKPOINT; 
+GO 
+DBCC DROPCLEANBUFFERS; 
+GO
+
+CREATE FULLTEXT CATALOG FullTextDescription
+
+CREATE FULLTEXT INDEX ON Logs(Description)
+KEY INDEX PK_Logs
+ON FullTextDescription
+WITH CHANGE_TRACKING AUTO
+SELECT Description 
+FROM LogEntries
+WHERE LogDate BETWEEN CONVERT(datetime,'2000-01-01') AND CONVERT(datetime,'2012-05-05')
+GO
+
+CHECKPOINT; 
+GO 
+DBCC DROPCLEANBUFFERS; 
+GO
+
+SELECT Description
+FROM LogEntries
+WHERE CONTAINS(Description,'abcdefg')
+GO
