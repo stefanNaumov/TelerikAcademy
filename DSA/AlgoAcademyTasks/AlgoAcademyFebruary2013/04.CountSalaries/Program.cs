a99e7ee5c38139f4b5da8449034cb8f133f46fab
stefanNaumov <stefannaumow@gmail.com>
2014-09-12 17:18:25 +0300

    solved LinkedOut task

--- DSA/AlgoAcademyTasks/AlgoAcademyFebruary2013/04.CountSalaries/Program.cs
+++ DSA/AlgoAcademyTasks/AlgoAcademyFebruary2013/04.CountSalaries/Program.cs
@@ -7,61 +7,63 @@ namespace _04.CountSalaries
 {
     class Program
     {
-        static int sumCount = 0;
         static bool[,] adjMatrix;
         static int numberOfEmployees;
         static bool[] usedEmployees;
-
+        static long[] sums;
+            
         static void Main()
         {
             numberOfEmployees = int.Parse(Console.ReadLine());
             adjMatrix = new bool[numberOfEmployees, numberOfEmployees];
+            sums = new long[numberOfEmployees];
 
             for (int i = 0; i < numberOfEmployees; i++)
             {
                 string currentUser = Console.ReadLine();
-                bool hasFoundEmployee = false;
 
                 for (int j = 0; j < numberOfEmployees; j++)
                 {
                     if (currentUser[j] == 'Y')
                     {
-                        hasFoundEmployee = true;
                         adjMatrix[i, j] = true;
                     }
                 }
-                //no slaves found
-                if (!hasFoundEmployee)
-                {
-                    sumCount += 1;
-                }
-            }
-            int sum = 0;
-            for (int i = 0; i < numberOfEmployees; i++)
-            {
-                for (int j = 0; j < numberOfEmployees; j++)
-                {
-                    if (adjMatrix[i,j])
-                    {
-                        sum += CountSums(j);
-                    }
-                }
             }
 
-            Console.WriteLine(sumCount);
+            long sum = 0;
+            for (int i = 0; i < numberOfEmployees; i++)
+            {
+                sum += CountSums(i);
+            }
+
+            Console.WriteLine(sum);
         }
 
-        int[] sums = new int[numberOfEmployees];
-
-        static int CountSums(int employee)
+        static long CountSums(int employee)
         {
-            int sum = 0;
+            if (sums[employee] > 0)
+            {
+                return sums[employee];
+            }
+            long sum = 0;
 
             for (int i = 0; i < numberOfEmployees; i++)
             {
-                
+                if (adjMatrix[employee, i])
+                {
+                    sum += CountSums(i);
+                }
             }
-            
+
+            if (sum == 0)
+            {
+                sum = 1;
+            }
+
+            sums[employee] = sum;
+
+            return sum;
         }
     }
 }
---
+++ DSA/AlgoAcademyTasks/AlgoAcademyFebruary2013/08.LinkedOut/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _08.LinkedOut
+{
+    class Person
+    {
+        public Person(string name)
+        {
+            this.Name = name;
+            this.Connetions = new List<Person>();
+        }
+
+        public Person(string name, int value)
+            :this(name)
+        {
+            this.Value = value;
+        }
+
+        public int Value { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Person> Connetions { get; set; }
+    }
+
+    class Program
+    {
+        static Dictionary<string, Person> people = new Dictionary<string, Person>();
+        static List<string> targets = new List<string>();
+        static HashSet<string> used = new HashSet<string>();
+        static string rootName;
+
+        static void Main()
+        {
+            rootName = Console.ReadLine();
+            //people.Add(rootName, new Person(rootName, 0));
+
+            ParseInput();
+
+            ProccessGraph();
+
+            PrintInput();
+        }
+
+        static void PrintInput()
+        {
+            foreach (var personName in targets)
+            {
+                if (!people.ContainsKey(personName))
+                {
+                    Console.WriteLine(-1);
+                }
+                else
+                {
+                    Console.WriteLine(people[personName].Value);
+                }
+                
+            }
+        }
+
+        static void ParseInput()
+        {
+            int numberOfConnections = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfConnections; i++)
+            {
+                string[] currConnection = Console.ReadLine().Split(new char[]{' '},StringSplitOptions.RemoveEmptyEntries);
+                string firstUserName = currConnection[0];
+                string secondUserName = currConnection[1];
+
+                if (!people.ContainsKey(firstUserName))
+                {
+                    people.Add(firstUserName, new Person(firstUserName));
+                }
+
+                if (!people.ContainsKey(secondUserName))
+                {
+                    people.Add(secondUserName, new Person(secondUserName));
+                }
+
+                people[firstUserName].Connetions.Add(people[secondUserName]);
+                people[secondUserName].Connetions.Add(people[firstUserName]);
+            }
+
+            int numberOfTargets = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfTargets; i++)
+            {
+                string targetName = Console.ReadLine();
+                targets.Add(targetName);
+            }
+        }
+
+        static void ProccessGraph()
+        {
+            Queue<string> queue = new Queue<string>();
+
+            queue.Enqueue(rootName);
+            used.Add(rootName);
+
+            while (queue.Count > 0)
+            {
+                Person currPerson = people[queue.Dequeue()];
+
+                foreach (Person person in currPerson.Connetions)
+                {
+                    if (!used.Contains(person.Name))
+                    {
+                        used.Add(person.Name);
+                        person.Value += currPerson.Value + 1; 
+                        queue.Enqueue(person.Name);
+                    }
+                }
+
+            }
+        }
+    }
+}
