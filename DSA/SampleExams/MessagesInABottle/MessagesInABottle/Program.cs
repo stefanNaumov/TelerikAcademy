01ae8e7d466e68484dc886a1c75651c95702b193
stefanNaumov <stefannaumow@gmail.com>
2014-09-14 14:36:36 +0300

    added exam preparation tasks from algo academy

--- DSA/SampleExams/MessagesInABottle/MessagesInABottle/Program.cs
+++ DSA/SampleExams/MessagesInABottle/MessagesInABottle/Program.cs
@@ -8,88 +8,96 @@ namespace MessagesInABottle
 {
     class Program
     {
-        static Dictionary<char, string> cipherValues = new Dictionary<char, string>();
-        static List<string> messageCombs = new List<string>();
-        static char[] currCombination = new char[60];
+        static Dictionary<char, string> letters;
+        static List<string> messages;
+        static char[] currMessage;
+        static int codeLength;
 
         static void Main()
         {
-            string message = Console.ReadLine();
-
-            string cipher = Console.ReadLine();
-
-            ParseCipher(cipher);
-
-            CountMessageCombs(message, 0, 0);
-
-            Console.WriteLine(messageCombs.Count);
-
-            messageCombs.Sort();
-
-            for (int i = 0; i < messageCombs.Count; i++)
-            {
-                var currStr = messageCombs[i];
-
-                for (int j = 0; j < currStr.Length; j++)
-                {
-                    Console.Write(currStr[j]);
-                }
-                Console.WriteLine();
-            }
+            letters = new Dictionary<char, string>();
             
+            string code = Console.ReadLine().Trim();
+            codeLength = code.Length;
+            messages = new List<string>();
+            currMessage = new char[50];
 
+            string cipher = Console.ReadLine().Trim();
+            ParseInput(cipher);
+
+            GenMessages(0, 0, code);
+
+            messages.Sort();
+
+            Console.WriteLine(messages.Count);
+            Print();
+
+            Console.WriteLine();
         }
 
-        static void ParseCipher(string cipher)
+        static void GenMessages(int index, int combinationIndex, string code)
         {
-            int index = 0;
-            char currLetter;
-            StringBuilder currValue = new StringBuilder();
-
-            while (index < cipher.Length)
+            if (index >= codeLength)
             {
-                if (cipher[index] >= 'A' && cipher[index] <= 'Z')
+                StringBuilder messageBuilder = new StringBuilder();
+                for (int i = 0; i < currMessage.Length; i++)
                 {
-                    currLetter = cipher[index];
-                    index++;
-
-                    while (index < cipher.Length && !(cipher[index] >= 'A' && cipher[index] <= 'Z'))
+                    if (!char.IsLetter(currMessage[i]))
                     {
-                        currValue.Append(cipher[index]);
-                        index++;
+                        break;
+                    }
+                    messageBuilder.Append(currMessage[i]);
+                }
+                messages.Add(messageBuilder.ToString() + "\n");
+                return;
+            }
+
+            foreach (var letter in letters)
+            {
+                if (letter.Value.Length + index <= codeLength && code.Substring(index, letter.Value.Length) == letter.Value)
+                {
+                    currMessage[combinationIndex] = letter.Key;
+                    GenMessages(index + letter.Value.Length, combinationIndex + 1, code);
+                    currMessage[combinationIndex] = char.MinValue;
+                }
+            }
+        }
+
+        static void Print()
+        {
+            for (int i = 0; i < messages.Count; i++)
+            {
+                Console.Write(messages[i]);
+            }
+        }
+
+        static void ParseInput(string cipher)
+        {
+            string currNumber;
+            char key = char.MinValue;
+            StringBuilder codeBuilder = new StringBuilder();
+
+            for (int i = 0; i < cipher.Length; i++)
+            {
+                if (char.IsLetter(cipher[i]))
+                {
+                    if (codeBuilder.Length > 0)
+                    {
+                        currNumber = codeBuilder.ToString();
+                        letters.Add(key, currNumber);
+                        codeBuilder.Clear();
                     }
 
-                    cipherValues.Add(currLetter, currValue.ToString());
-                    currValue.Clear();
-                }    
-            }
-        }
-
-        static void CountMessageCombs(string message, int index, int combinationIndex)
-        {
-            if (index == message.Length)
-            {
-                StringBuilder builder = new StringBuilder();
-                for (int i = 0; i < currCombination.Length; i++)
-                {
-                    builder.Append(currCombination[i]);
+                    key = cipher[i];
                 }
-                messageCombs.Add(builder.ToString());
-
-                return;
-            }
-            if (index > message.Length)
-            {
-                return;
-            }
-
-            foreach (var item in cipherValues)
-            {
-                if (item.Value.Length + index <= message.Length && message.Substring(index,item.Value.Length) == item.Value)
+                else
                 {
-                    currCombination[combinationIndex] = item.Key;
-                    CountMessageCombs(message, index + item.Value.Length, combinationIndex + 1);
-                    currCombination[combinationIndex] = char.MinValue;
+                    codeBuilder.Append(cipher[i]);
+                }
+
+                if (i == cipher.Length - 1)
+                {
+                    letters.Add(key, codeBuilder.ToString());
                 }
             }
         }
