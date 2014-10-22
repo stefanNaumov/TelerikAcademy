cceb7ea2a3473285c5267ece27bf809f43a0f7c2
stefannaumov <stefannaumow@gmail.com>
2014-10-22 05:00:46 -0700

    added Objective C methods

---
+++ MobileDevelopment/iOS/ObjectiveC-methods/ObjectiveC-methods/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjectiveC-methods
+//
+//  Created by admin on 10/22/14.
+//  Copyright (c) 2014 admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
