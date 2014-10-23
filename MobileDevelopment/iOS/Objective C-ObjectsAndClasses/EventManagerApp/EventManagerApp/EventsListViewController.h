b65c360c46e2017422ba1b9116eefa4ba1012fec
stefannaumov <stefannaumow@gmail.com>
2014-10-23 15:25:37 -0700

    added simple Event Manager

---
+++ MobileDevelopment/iOS/Objective C-ObjectsAndClasses/EventManagerApp/EventManagerApp/EventsListViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventsListViewController.h
+//  EventManagerApp 07-07-36-812
+//
+//  Created by admin on 10/23/14.
+//  Copyright (c) 2014 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventsListViewController : UIViewController
+
+@end
---
+++ MobileDevelopment/iOS/Objective C-ObjectsAndClasses/EventManagerApp/EventManagerApp/EventsListViewController.m
@@ -0,0 +1,37 @@
+//
+//  EventsListViewController.m
+//  EventManagerApp 07-07-36-812
+//
+//  Created by admin on 10/23/14.
+//  Copyright (c) 2014 admin. All rights reserved.
+//
+
+#import "EventsListViewController.h"
+
+@interface EventsListViewController ()
+
+@end
+
+@implementation EventsListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
