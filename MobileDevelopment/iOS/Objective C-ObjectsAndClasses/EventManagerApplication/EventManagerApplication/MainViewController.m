2546b9ba6184504af8303a8ec2ee1574bbce7cf5
stefannaumov <stefannaumow@gmail.com>
2014-10-28 15:22:21 -0700

    add keyboard removing

--- MobileDevelopment/iOS/Objective C-ObjectsAndClasses/EventManagerApplication/EventManagerApplication/MainViewController.m
+++ MobileDevelopment/iOS/Objective C-ObjectsAndClasses/EventManagerApplication/EventManagerApplication/MainViewController.m
@@ -12,6 +12,8 @@
 
 @end
 
+#define trimAll(object)[object stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]]
+
 @implementation MainViewController
 
 - (void)viewDidLoad {
@@ -29,21 +31,29 @@
 }
 
 - (IBAction)addEvent:(id)sender {
-    
     NSString *eventTitle = self.eventTitleInput.text;
-    NSDate *eventDate = self.datePicker.date;
     
-    Event *newEvent = [[Event alloc] init];
-    [newEvent setTitle:eventTitle];
-    [newEvent setDate:eventDate];
-    
-    [self.eventManager createEvent:newEvent];
-    
-    //clear input field
-    self.eventTitleInput.text = @"";
+    if ([trimAll(eventTitle) length] > 0) {
+        NSDate *eventDate = self.datePicker.date;
+        Event *newEvent = [[Event alloc] init];
+        [newEvent setTitle:eventTitle];
+        [newEvent setDate:eventDate];
+        
+        [self.eventManager createEvent:newEvent];
+        
+        //clear input field
+        self.eventTitleInput.text = @"";
+        
+        UIAlertView *alertView = [[UIAlertView alloc]initWithTitle:@"Message" message:@"Event added to list" delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles:nil, nil];
+        [alertView show];
+    }
     
     [self.eventTitleInput resignFirstResponder];
     
+}
+
+-(void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    [self.view endEditing:TRUE];
 }
 
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
