4cdd5ef159b2ebb558c8cf47b81607ba4e4842b0
stefannaumov <stefannaumow@gmail.com>
2014-11-03 13:07:20 -0800

    started HttpRequestHandler

---
+++ MobileDevelopment/iOS/iOS-Views/HttpRequests/HttpRequests/HttpRequestHandler.h
@@ -0,0 +1,13 @@
+//
+//  HttpRequestHandler.h
+//  HttpRequests
+//
+//  Created by admin on 11/3/14.
+//  Copyright (c) 2014 admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HttpRequestHandler : NSObject
+
+@end
---
+++ MobileDevelopment/iOS/iOS-Views/HttpRequests/HttpRequests/HttpRequestHandler.m
@@ -0,0 +1,13 @@
+//
+//  HttpRequestHandler.m
+//  HttpRequests
+//
+//  Created by admin on 11/3/14.
+//  Copyright (c) 2014 admin. All rights reserved.
+//
+
+#import "HttpRequestHandler.h"
+
+@implementation HttpRequestHandler
+
+@end
